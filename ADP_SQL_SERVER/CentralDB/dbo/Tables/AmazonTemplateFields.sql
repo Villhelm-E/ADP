cf6c4d2234ec2b134e02421271c93b8c4d3b89b6
Villhelm-E <villhelm.e@gmail.com>
2022-05-24 14:43:51 -0700

    Added Master Database Access file

--- ADP_SQL_SERVER/CentralDB/dbo/Tables/AmazonTemplateFields.sql
+++ ADP_SQL_SERVER/CentralDB/dbo/Tables/AmazonTemplateFields.sql
@@ -4,7 +4,7 @@
     [Display_Name] NVARCHAR(255) NULL, 
     [Label_Name] NVARCHAR(255) NULL, 
     [Field_Name] NVARCHAR(255) NULL, 
-    [Definition_and_Use] NVARCHAR(255) NULL, 
+    [Definition_and_Use] NVARCHAR(MAX) NULL, 
     [Accepted_Values] NVARCHAR(255) NULL, 
     [Organization] NVARCHAR(255) NULL, 
     [Active] BIT NULL, 
