e9480125a73cf18ec332c9a8501b608e65ff9313
Villhelm-E <villhelm.e@gmail.com>
2022-06-20 14:50:06 -0700

    Added Sixbit server stand-in

---
+++ SIXBITDBSERVER/Sixbit/dbo/Tables/dbo.CompatibilitySets.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CompatibilitySets] (
+    [CompatibilitySetID]         INT           IDENTITY (1, 1) NOT NULL,
+    [Name]                       NVARCHAR (255) NULL,
+    [CategoryID]                 INT           NULL,
+    [CompatibilitySetDefinition] NVARCHAR (MAX) NULL,
+    [OrdinalID]                  INT           NULL
+);
